88303f511a618f9e02c6668039b5669a18ff4c6a
rio.coder <sarfaraztech07@gmail.com>
2025-03-09 12:40:46 +0530

    start navbar done

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+    } else {
+    menu.classList.add('hidden');
+    }
+    });
+    
