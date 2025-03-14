123058b4dc5009a34b3bca7fd672fe3c9a5116c8
rio.coder <sarfaraztech07@gmail.com>
2025-03-15 00:41:48 +0530

    update hero section

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu-button').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.classList.contains('hidden')) {
-    menu.classList.remove('hidden');
-    } else {
-    menu.classList.add('hidden');
-    }
-    });
-    
